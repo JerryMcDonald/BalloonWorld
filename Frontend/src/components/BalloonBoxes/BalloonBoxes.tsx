91fb3434db029c99787593991626f1485c21a8fc
Jerry McDonald <jmcdonald.dad.dev@gmail.com>
2024-01-09 13:50:35 -0600

    Move the logic for hitting the boxes into the checkCollision function. Add a locked check

--- Frontend/src/components/BalloonBoxes/BalloonBoxes.tsx
+++ Frontend/src/components/BalloonBoxes/BalloonBoxes.tsx
@@ -22,33 +22,60 @@ const BalloonBoxes: React.FC<BalloonBoxesProps> = ({ left, playerPosition, jumpS
 
   // Calculate the left and right bounds of this component's boxes
   // Although not a heavy computation, useMemo is good practice here because it will prevent unnecessary re-renders, also useMemo will clearly indicate that boxBounds should only update if left is changed. 
-    const boxBounds = useMemo(() => ({
-      boxOne: { start: left, end: left + 30 },
-      boxTwo: { start: left + 90, end: left + 120 },
-      boxThree: { start: left + 180, end: left + 210 },
-      boxFour: { start: left + 270, end: left + 300 }
-    }), [left]);
+  const boxBounds = useMemo(() => ({
+    boxOne: { start: left, end: left + 30 },
+    boxTwo: { start: left + 90, end: left + 120 },
+    boxThree: { start: left + 180, end: left + 210 },
+    boxFour: { start: left + 270, end: left + 300 }
+  }), [left]);
 
-
-  const colors = ['red', 'green', 'orange', 'blue'];
-  const nextColor = () => colors[(colors.indexOf(currentColor) + 1) % colors.length];
+  const nextColor = useCallback(() => {
+    const colors = ['red', 'green', 'orange', 'blue'];
+    return colors[(colors.indexOf(currentColor) + 1) % colors.length];
+  }, [currentColor]);
 
   const checkCollision = useCallback(() => {
-    // Check if the player is within this component before performing additional logic
     if (jumpStage === jumpPeakStage && playerRight >= boxBounds.boxOne.start && playerLeft <= boxBounds.boxFour.end) {
       console.log('Player is within a BallonBoxes Component', left);
-      // next determine which box was hit
-      if (playerLeft <= boxBounds.boxOne.end) {
-        console.log('touching box one')
-      } else if (playerRight >= boxBounds.boxTwo.start && playerLeft <= boxBounds.boxTwo.end) {
-        console.log('touching box two')
-      } else if (playerRight >= boxBounds.boxThree.start && playerLeft <= boxBounds.boxThree.end) {
-        console.log('touching box three')
-      } else if (playerRight >= boxBounds.boxFour.start) { // no need to check playerLeft due to parent if statement
-        console.log('touching box four')
+
+      // Box One Collision
+      if (playerRight >= boxBounds.boxOne.start && playerLeft <= boxBounds.boxOne.end) {
+        if (!isUnlocked) {
+          setIsUnlocked(true);
+          setShowBalloon(true);
+          console.log('touching box one');
+        }
+      }
+
+      // Box Two Collision
+      else if (playerRight >= boxBounds.boxTwo.start && playerLeft <= boxBounds.boxTwo.end) {
+        if (isUnlocked) {
+          setCurrentColor(nextColor());
+          console.log('touching box two');
+        }
+      }
+
+      // Box Three Collision
+      else if (playerRight >= boxBounds.boxThree.start && playerLeft <= boxBounds.boxThree.end) {
+        if (isUnlocked) {
+          setBalloonColor(currentColor);
+          console.log('touching box three');
+        }
+      }
+
+      // Box Four Collision
+      else if (playerRight >= boxBounds.boxFour.start && playerLeft <= boxBounds.boxFour.end) {
+        if (isUnlocked) {
+          setIsUnlocked(false);
+          setShowBalloon(false);
+          setCurrentColor('red');
+          console.log('touching box four');
+        }
       }
     }
-  }, [jumpStage, left, boxBounds, playerLeft, playerRight]);
+  }, [jumpStage, playerLeft, playerRight, boxBounds, isUnlocked, currentColor, nextColor, left]);
+
+
   useEffect(() => {
     checkCollision();
   }, [checkCollision]);
@@ -57,33 +84,29 @@ const BalloonBoxes: React.FC<BalloonBoxesProps> = ({ left, playerPosition, jumpS
   return (
     <div className="balloon-box-container" style={{ left: `${left}px` }}>
       {/* First Box */}
-      <div 
-        className="box" 
-        style={{ backgroundColor: isUnlocked ? 'successGreen' : 'black' }} 
-        onClick={() => { setIsUnlocked(true); setShowBalloon(true); }}
+      <div
+        className="box"
+        style={{ backgroundColor: isUnlocked ? 'successGreen' : 'black' }}
       ></div>
       {/* Second Box */}
-      <div 
-        className="box" 
-        style={{ backgroundColor: isUnlocked ? currentColor : 'grey', marginLeft: '60px' }} 
-        onClick={() => { if (isUnlocked) setCurrentColor(nextColor()); }}
+      <div
+        className="box"
+        style={{ backgroundColor: isUnlocked ? currentColor : 'grey', marginLeft: '60px' }}
       ></div>
       {/* Third Box */}
-      <div 
-        className="box" 
-        style={{ backgroundColor: isUnlocked ? 'blue' : 'grey', marginLeft: '60px' }} 
-        onClick={() => { if (isUnlocked) setBalloonColor(currentColor); }}
+      <div
+        className="box"
+        style={{ backgroundColor: isUnlocked ? 'blue' : 'grey', marginLeft: '60px' }}
       ></div>
       {/* Fourth Box */}
-      <div 
-        className="box" 
-        style={{ backgroundColor: isUnlocked ? 'red' : 'grey', marginLeft: '60px' }} 
-        onClick={() => { setIsUnlocked(false); setShowBalloon(false); setCurrentColor('red'); }}
+      <div
+        className="box"
+        style={{ backgroundColor: isUnlocked ? 'red' : 'grey', marginLeft: '60px' }}
       ></div>
       {/* Balloon */}
       {showBalloon && (
-        <div 
-          className="balloon" 
+        <div
+          className="balloon"
           style={{ backgroundColor: balloonColor, top: '-100px' }}
         ></div>
       )}
