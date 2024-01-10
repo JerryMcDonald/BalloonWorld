4bf6c26c1c931dfd6e80a4252ca344274571889b
Jerry McDonald <jmcdonald.dad.dev@gmail.com>
2024-01-09 23:23:36 -0600

    Add the BalloonWorldContext to Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<BalloonWorldContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
