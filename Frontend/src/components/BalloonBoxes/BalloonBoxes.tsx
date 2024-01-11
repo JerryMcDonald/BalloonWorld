48d50164f2d63697d2249bacf6a277eb1e6f96b2
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2024-01-11 14:28:47 -0600

    Improve the state management of my BalloonBoxes component by using the balloonData state directly for all operations instead of useRef. What a relief!

--- Frontend/src/components/BalloonBoxes/BalloonBoxes.tsx
+++ Frontend/src/components/BalloonBoxes/BalloonBoxes.tsx
@@ -1,43 +1,34 @@
-// src/components/BalloonBoxes/BalloonBoxes.tsx
-
-import React, { useState, useEffect, useCallback, useMemo, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import './BalloonBoxes.css';
 import Guardian from '../Guardian/Guardian';
 import { addBalloon, deleteBalloon, updateBalloon } from '../../services/balloonService';
-
 
 // Define a type for the component props
 interface BalloonBoxesProps {
   left: number;
   playerPosition: number;
   jumpStage: number;
-  initialBalloonStatus:string;
+  initialBalloonStatus: string;
   initialBalloonId: number;
   guardian: string;
   updateGuardians: (id: number, guardian: string, color: string) => void;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const BalloonBoxes: React.FC<BalloonBoxesProps> = ({ left, playerPosition, jumpStage, initialBalloonStatus, initialBalloonId, guardian, updateGuardians }) => {
-  const [isUnlocked, setIsUnlocked] = useState(false);
-  const [balloonColor, setBalloonColor] = useState('red');
-  const [blockTwoColor, setBlockTwoColor] = useState('red');
-  const [showBalloon, setShowBalloon] = useState(false);
-  // const [balloonId, setBalloonId] = useState(initialBalloonId)
+  const [balloonData, setBalloonData] = useState({
+    isUnlocked: initialBalloonStatus !== 'noBalloon',
+    balloonColor: initialBalloonStatus !== 'noBalloon' ? initialBalloonStatus : 'red',
+    blockTwoColor: initialBalloonStatus !== 'noBalloon' ? initialBalloonStatus : 'red',
+    showBalloon: initialBalloonStatus !== 'noBalloon',
+    balloonId: initialBalloonId,
+  });
 
   const playerLeft = playerPosition; // this is unnecessary but improves readability
   const playerRight = playerPosition + 30; // the player is 30px wide
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const jumpPeakStage = 2; // 2 is the peak of the jump
 
-  const isUnlockedRef = useRef(isUnlocked);
-  const balloonColorRef = useRef(balloonColor);
-  const blockTwoColorRef = useRef(blockTwoColor);
-  const showBalloonRef = useRef(showBalloon);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const balloonIdRef = useRef(initialBalloonId)
-
   // Calculate the left and right bounds of this component's boxes
-  // Although not a heavy computation, useMemo is good practice here because it will prevent unnecessary re-renders, also useMemo will clearly indicate that boxBounds should only update if left is changed. 
   const boxBounds = useMemo(() => ({
     boxOne: { start: left, end: left + 30 },
     boxTwo: { start: left + 90, end: left + 120 },
@@ -47,26 +38,27 @@ const BalloonBoxes: React.FC<BalloonBoxesProps> = ({ left, playerPosition, jumpS
 
   // Determine the guardian's color
   const guardianColor = guardian === 'BalloonKing' ? 'green' :
-                        guardian === 'BalloonGenie' ? 'purple' : 'blue';
-
-
+    guardian === 'BalloonGenie' ? 'purple' : 'blue';
 
   const checkCollision = useCallback(async (playerLeft: number, playerRight: number) => {
     if (playerRight >= boxBounds.boxOne.start && playerLeft <= boxBounds.boxFour.end) {
 
       // Box One Collision
       if (playerRight >= boxBounds.boxOne.start && playerLeft <= boxBounds.boxOne.end) {
-        if (!isUnlocked) {
-          isUnlockedRef.current = true;
-          balloonColorRef.current = blockTwoColorRef.current;
-          showBalloonRef.current = true;
+        if (!balloonData.isUnlocked) {
           try {
-            const response = await addBalloon({color: blockTwoColorRef.current, guardian: guardian });
+            const response = await addBalloon({ color: balloonData.blockTwoColor, guardian: guardian });
             if (response.id) {
-              balloonIdRef.current = response.id
-              updateGuardians(response.id, guardian, blockTwoColorRef.current)
+              const int = response.id
+              setBalloonData(prevData => ({
+                ...prevData,
+                isUnlocked: true,
+                showBalloon: true,
+                balloonColor: balloonData.blockTwoColor,
+                balloonId: int
+              }));
+              updateGuardians(response.id, guardian, balloonData.blockTwoColor);
             }
-            console.log(response, 'added balloon')
           } catch (error) {
             console.error('Failed to add balloon:', error);
           }
@@ -75,135 +67,114 @@ const BalloonBoxes: React.FC<BalloonBoxesProps> = ({ left, playerPosition, jumpS
 
       // Box Two Collision - change the color of the block
       else if (playerRight >= boxBounds.boxTwo.start && playerLeft <= boxBounds.boxTwo.end) {
-        if (isUnlocked) {
+        if (balloonData.isUnlocked) {
           const colors = ['red', 'green', 'orange', 'blue', 'purple'];
-          if (blockTwoColorRef.current === balloonColorRef.current) {
-            blockTwoColorRef.current = colors[(colors.indexOf(blockTwoColorRef.current) + 1) % colors.length];
-
-          } else {
-            // remove the current balloon color from the choices
-            const newColors = colors.filter(color => color !== balloonColorRef.current)
-            blockTwoColorRef.current = newColors[(newColors.indexOf(blockTwoColorRef.current) + 1) % newColors.length];
-          }
-
+          const newBlockTwoColor = colors[(colors.indexOf(balloonData.blockTwoColor) + 1) % colors.length];
+          setBalloonData(prevData => ({
+            ...prevData,
+            blockTwoColor: newBlockTwoColor
+          }));
         }
       }
 
       // Box Three Collision
       else if (playerRight >= boxBounds.boxThree.start && playerLeft <= boxBounds.boxThree.end) {
-        if (isUnlocked) {
-          balloonColorRef.current = blockTwoColorRef.current;
-          showBalloonRef.current = true;
-          if (balloonIdRef.current) {
-            try {
-              await updateBalloon({ id: balloonIdRef.current, color: blockTwoColorRef.current, guardian: guardian } );
-              updateGuardians(balloonIdRef.current, guardian, blockTwoColorRef.current)
-              console.log('updated balloon')
-            } catch (error) {
-              console.error('Failed to update balloon:', error);
-              // Handle the error appropriately
-            }
+        if (balloonData.isUnlocked && balloonData.balloonId) {
+          try {
+            await updateBalloon({ id: balloonData.balloonId, color: balloonData.blockTwoColor, guardian: guardian });
+            setBalloonData(prevData => ({
+              ...prevData,
+              balloonColor: balloonData.blockTwoColor
+            }));
+            updateGuardians(balloonData.balloonId, guardian, balloonData.blockTwoColor);
+          } catch (error) {
+            console.error('Failed to update balloon:', error);
           }
-          
         }
       }
-
-    
 
       // Box Four Collision
       else if (playerRight >= boxBounds.boxFour.start && playerLeft <= boxBounds.boxFour.end) {
-        if (isUnlocked) {
-          isUnlockedRef.current = false;
-          showBalloonRef.current = false;
-          if (balloonIdRef.current) {
-            try {
-              await deleteBalloon(balloonIdRef.current);
-              updateGuardians(balloonIdRef.current, guardian, 'noBalloon')
-
-              console.log('deleted balloon')
-            } catch (error) {
-              console.error('Failed to delete balloon:', error);
-              // Handle the error appropriately
-            }
+        if (balloonData.isUnlocked && balloonData.balloonId) {
+          try {
+            await deleteBalloon(balloonData.balloonId);
+            setBalloonData(prevData => ({
+              ...prevData,
+              isUnlocked: false,
+              showBalloon: false,
+              balloonId: 0
+            }));
+            updateGuardians(balloonData.balloonId, guardian, 'noBalloon');
+          } catch (error) {
+            console.error('Failed to delete balloon:', error);
           }
         }
       }
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [boxBounds, isUnlocked, guardian]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [boxBounds, guardian, updateGuardians]);
 
   useEffect(() => {
     if (initialBalloonStatus !== 'noBalloon') {
-      showBalloonRef.current = true;
-      setShowBalloon(true);
-      isUnlockedRef.current = true;
-      setIsUnlocked(true);
-      balloonColorRef.current = initialBalloonStatus
-      setBalloonColor(initialBalloonStatus)
-      blockTwoColorRef.current = initialBalloonStatus
-      setBlockTwoColor(initialBalloonStatus)
-      balloonIdRef.current = initialBalloonId
-      
-      // No need to directly set states here, as refs are updated in checkCollision
+      setBalloonData({
+        isUnlocked: true,
+        balloonColor: initialBalloonStatus,
+        blockTwoColor: initialBalloonStatus,
+        showBalloon: true,
+        balloonId: initialBalloonId,
+      });
     }
   }, [initialBalloonStatus, initialBalloonId]);
 
+
+  // React to changes in the game state such as jumpStage
   useEffect(() => {
-    if (jumpStage === jumpPeakStage) {
+    if (jumpStage === 2) { // Assuming 2 is the peak of the jump
       checkCollision(playerLeft, playerRight);
-      // No need to directly set states here, as refs are updated in checkCollision
     }
   }, [jumpStage, playerLeft, playerRight, checkCollision]);
 
-  useEffect(() => {
-    if (jumpStage === jumpPeakStage) {
-      // Update actual states with values from refs
-      setIsUnlocked(isUnlockedRef.current);
-      setShowBalloon(showBalloonRef.current);
-      setBlockTwoColor(blockTwoColorRef.current);
-      if (balloonColorRef.current !== balloonColor) {
-        // updateStatus(guardian, balloonColorRef.current);
-        setBalloonColor(balloonColorRef.current);
-        // updateStatus(guardian, balloonColorRef.current, showBalloonRef.current);
-      }
-    }
-  }, [jumpStage, guardian, balloonColor]);
-
   return (
     <div className="balloon-box-container" style={{ left: `${left}px` }}>
+      {/* Guardian */}
+      <Guardian
+        // type={guardian}
+        color={guardianColor}
+        hasBalloon={balloonData.showBalloon}
+        favoriteBalloon={balloonData.balloonColor === guardianColor}
+      />
+
       {/* First Box */}
       <div
         className="box"
-        style={{ backgroundColor: isUnlocked ? 'successGreen' : 'black' }}
+        style={{ backgroundColor: balloonData.isUnlocked ? 'successGreen' : 'black' }}
       ></div>
+
       {/* Second Box */}
       <div
         className="box"
-        style={{ backgroundColor: isUnlocked ? blockTwoColor : 'grey', marginLeft: '60px' }}
+        style={{ backgroundColor: balloonData.isUnlocked ? balloonData.blockTwoColor : 'grey', marginLeft: '60px' }}
       ></div>
+
       {/* Third Box */}
       <div
         className="box"
-        style={{ backgroundColor: isUnlocked ? 'blue' : 'grey', marginLeft: '60px' }}
+        style={{ backgroundColor: balloonData.isUnlocked ? 'blue' : 'grey', marginLeft: '60px' }}
       ></div>
+
       {/* Fourth Box */}
       <div
         className="box"
-        style={{ backgroundColor: isUnlocked ? 'red' : 'grey', marginLeft: '60px' }}
+        style={{ backgroundColor: balloonData.isUnlocked ? 'red' : 'grey', marginLeft: '60px' }}
       ></div>
+
       {/* Balloon */}
-      {showBalloon && (
+      {balloonData.showBalloon && (
         <div
           className="balloon"
-          style={{ backgroundColor: balloonColor, top: '-100px' }}
+          style={{ backgroundColor: balloonData.balloonColor, top: '-100px' }}
         ></div>
       )}
-      {/* Guardian */}
-      <Guardian
-        color={guardianColor}
-        hasBalloon={showBalloon}
-        favoriteBalloon={balloonColor === guardianColor}
-      />
     </div>
   );
 };
