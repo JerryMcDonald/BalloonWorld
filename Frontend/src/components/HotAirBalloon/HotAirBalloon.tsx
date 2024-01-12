f8ce4da8aca5f7ff131cbab902a2c3a4cc3a469a
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2024-01-12 02:58:01 -0600

    replace placeholder hot air balloon size and css

--- Frontend/src/components/HotAirBalloon/HotAirBalloon.tsx
+++ Frontend/src/components/HotAirBalloon/HotAirBalloon.tsx
@@ -2,6 +2,8 @@
 
 import React, { useState, useEffect } from 'react';
 import './HotAirBalloon.css';
+import HotAirBalloonEmpty from '../../assets/hotAirBalloon/HotAirBalloonEmpty.png';
+import HotAirBalloonWithGerald from '../../assets/hotAirBalloon/HotAirBalloonWithGerald.png';
 
 interface HotAirBalloonProps {
   playerPosition: number;
@@ -10,16 +12,21 @@ interface HotAirBalloonProps {
   onPlayerJumpOn: () => void;
 }
 
-const HotAirBalloon: React.FC<HotAirBalloonProps> = ({ playerPosition, jumpStage, playerWidth, onPlayerJumpOn}) => {
+const HotAirBalloon: React.FC<HotAirBalloonProps> = ({ playerPosition, jumpStage, playerWidth, onPlayerJumpOn }) => {
   const [isLifted, setIsLifted] = useState(false);
-  const balloonPosition = 4800;
-  const jumpPeakStage = 2;
+  const balloonPosition: number = 4400;
+  const jumpPeakStage: number = 2;
 
   useEffect(() => {
+    const AwesomeAfroLeft: number = playerPosition + 50; // players awesome afro starts at about 50px in from playerPosition
+    const AwesomeAfroRight: number = playerPosition + 80; // players awesome afro is about 30px wide
+    const HotAirBalloonLeft: number = balloonPosition + 150; // hot air balloon starts at about 50px in from playerPosition
+    const HotAirBalloonRight: number = balloonPosition + 350; // hot air balloon is about 200px wide
+
     // Check if player is in front of the balloon and jumps
-    if (jumpStage === jumpPeakStage && playerPosition >= balloonPosition - playerWidth && playerPosition <= balloonPosition + 100) {
+    if (jumpStage === jumpPeakStage && AwesomeAfroRight >= HotAirBalloonLeft && AwesomeAfroLeft <= HotAirBalloonRight) {
       setIsLifted(true);
-      onPlayerJumpOn(); // Call the callback
+      onPlayerJumpOn(); // make the player disappear
       setTimeout(() => {
         window.location.reload(); // Refresh the page after the balloon is out of view
       }, 2000); // Adjust time based on animation duration
@@ -28,7 +35,8 @@ const HotAirBalloon: React.FC<HotAirBalloonProps> = ({ playerPosition, jumpStage
 
   return (
     <div className={`hot-air-balloon ${isLifted ? 'lift-off' : ''}`} style={{ left: `${balloonPosition}px` }}>
-      {isLifted && <div className="player-on-balloon"></div>}
+      {isLifted && <img src={HotAirBalloonWithGerald} alt="HAB Image" />
+      || <img src={HotAirBalloonEmpty} alt="HAB Image" />}
     </div>
   );
 };
