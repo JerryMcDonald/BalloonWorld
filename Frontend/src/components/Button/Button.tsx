62e886d36d206d1888b04421037500a9cefbeaed
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2024-01-11 20:14:02 -0600

    Button Component and css

---
+++ Frontend/src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+// src/components/Button/Button.tsx
+
+import React from 'react';
+import './Button.css';
+import RedSwap from '../../assets/buttons/RedSwap.png'
+
+
+const Button: React.FC = () => {
+  return (
+    <div>
+        <div className="Button">
+
+        <img src={ButtonFullWithMountians}></img>
+        </div>
+
+    </div>
+  );
+};
+
+export default Button;
